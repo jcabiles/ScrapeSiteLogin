d190f3e7f4a1a487b1f1ab30be08ede866975d4c
John Cabiles <johncabiles@gmail.com>
2020-05-09 21:48:43 -0700

    Set up logging in via site homepage form

--- mechsoup.py
+++ mechsoup.py
@@ -2,16 +2,9 @@ import mechanicalsoup
 import yaml
 
 config = yaml.safe_load(open('config.yml'))
-
-# Connect to duckduckgo
-browser = mechanicalsoup.StatefulBrowser(user_agent="MechanicalSoup")
+browser = mechanicalsoup.StatefulBrowser()
 browser.open(config['site'])
 
-# Fill-in the search form
-browser.select_form('#search_form_homepage')
-browser["q"] = "MechanicalSoup"
-browser.submit_selected()
-
-# Display the results
-for link in browser.page.select('a.result__a'):
-    print(link.text, '->', link.attrs['href'])
+form = browser.select_form()
+form.set_input({"log": config['username'],
+                "pwd": config['password']})
