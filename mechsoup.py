30164eb05f2c2807cb7dfdbe73e6a7dc140814e0
John Cabiles <johncabiles@gmail.com>
2020-05-09 21:50:15 -0700

    Add documentation for login

--- mechsoup.py
+++ mechsoup.py
@@ -5,6 +5,12 @@ config = yaml.safe_load(open('config.yml'))
 browser = mechanicalsoup.StatefulBrowser()
 browser.open(config['site'])
 
+"""
+Get login form to enter login and password.
+
+Note that you'll need to need to open up your browser's dev tools
+in order to find what username_login and password fields are called.
+"""
 form = browser.select_form()
 form.set_input({"log": config['username'],
                 "pwd": config['password']})
