5e2f6727ea70f690df6b50e5a67d75132ea1932e
John Cabiles <johncabiles@gmail.com>
2020-05-10 12:21:44 -0700

    Login and fetch URL

--- mechsoup.py
+++ mechsoup.py
@@ -12,5 +12,11 @@ Note that you'll need to need to open up your browser's dev tools
 in order to find what username_login and password fields are called.
 """
 form = browser.select_form()
-form.set_input({"log": config['username'],
-                "pwd": config['password']})
+form.input({"log": config['username'],
+            "pwd": config['password']})
+browser.submit_selected()
+browser.get_url()
+
+#browser.get_current_page()
+
+
