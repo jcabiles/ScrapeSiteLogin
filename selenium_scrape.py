d9ad737dd570239fb39a9fd696f8bf8dbe1a784d
John Cabiles <johncabiles@gmail.com>
2020-05-10 12:59:07 -0700

    Enter login credentials

--- selenium_scrape.py
+++ selenium_scrape.py
@@ -7,5 +7,9 @@ import os
 config = yaml.safe_load(open('config.yml'))
 driver = webdriver.Chrome()
 driver.get(config['site'])
+driver.find_element_by_name("modal-button").click()
 
-
+# enter credentials
+driver.find_element_by_id("log").send_keys(config['username'])
+driver.find_element_by_id("pwd").send_keys(config['password'])
+driver.find_element_by_name("log in").click()
