6e37b8a5ba8eb10cc7659bd14812ab2ac998f23e
John Cabiles <johncabiles@gmail.com>
2020-05-10 21:57:26 -0700

    Add option to run Chrome headless mode

--- selenium_scrape.py
+++ selenium_scrape.py
@@ -3,13 +3,22 @@ from selenium.webdriver.common.keys import Keys
 import yaml
 import os
 
-# instantiate webdriver and find login button
 config = yaml.safe_load(open('config.yml'))
-driver = webdriver.Chrome()
+
+# when ready, add lines to let selenium run w/o opening browser
+# op = webdriver.ChromeOptions()
+# op.add_argument('headless')
+
+# instantiate webdriver and find login button
+driver = webdriver.Chrome(#options=op
+     )
 driver.get(config['site'])
-driver.find_element_by_name("modal-button").click()
+driver.find_element_by_xpath(
+    '//*[@id="masthead"]/div/div/div/div/div[2]/ul/li/a[1]').click()
 
 # enter credentials
-driver.find_element_by_id("log").send_keys(config['username'])
-driver.find_element_by_id("pwd").send_keys(config['password'])
-driver.find_element_by_name("log in").click()
+driver.find_element_by_name("log").send_keys(config['username'])
+driver.find_element_by_name("pwd").send_keys(config['password'])
+driver.find_element_by_xpath(
+    '//*[@id="login-modal"]/div/div/form/button').click()
+
